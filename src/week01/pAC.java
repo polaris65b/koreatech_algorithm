0105c4a498be39e16a4d5ae5adda19b95d01a5b3
polaris65b <42924432+polaris65b@users.noreply.github.com>
2024-03-27 15:03:10 +0900

    t

--- src/week01/pAC.java
+++ src/week01/pAC.java
@@ -37,7 +37,7 @@ public class pAC {
     public static int find(int[] seq) {
         int max = 0;
 
-        for (int i = 0; i < seq.length - 1; i++) {
+        for (int i = 0; i < seq.length - 1; i++){
             if (seq[i] > 0 && seq[i + 1] > 0) {
                 max = Math.max(max, seq[i] + seq[i + 1]);
             }
